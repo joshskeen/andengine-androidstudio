74af56e7451478686d567d9bc3b0136fc34376c8
Josh Skeen <josh@joshskeen.com>
2014-11-25 12:34:41 -0500

    initial commit

---
+++ app/src/main/java/com/joshskeen/andengine/MainActivity.java
@@ -0,0 +1,28 @@
+package com.joshskeen.andengine;
+
+import org.andengine.engine.options.EngineOptions;
+import org.andengine.ui.activity.BaseGameActivity;
+
+
+public class MainActivity extends BaseGameActivity {
+
+    @Override
+    public EngineOptions onCreateEngineOptions() {
+        return null;
+    }
+
+    @Override
+    public void onCreateResources(OnCreateResourcesCallback pOnCreateResourcesCallback) throws Exception {
+
+    }
+
+    @Override
+    public void onCreateScene(OnCreateSceneCallback pOnCreateSceneCallback) throws Exception {
+
+    }
+
+    @Override
+    public void onPopulateScene(org.andengine.entity.scene.Scene pScene, OnPopulateSceneCallback pOnPopulateSceneCallback) throws Exception {
+
+    }
+}
